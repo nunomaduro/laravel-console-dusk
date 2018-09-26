6acadbcd2fa1801eaa34d400776ef58824f6a9e1
Claudiu Nicola <claudiunicola96@gmail.com>
2018-09-26 12:59:12 +0300

    I added comments on the config file

--- config/console-dusk.php
+++ config/console-dusk.php
@@ -1,10 +1,25 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel Console Dusk Paths
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the name of screenshots and logs directory as you wish.
+    */
     'paths' => [
         'screenshots' => 'screenshots',
         'log'         => 'log',
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel Console Dusk Secret
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the output of command.
+    | It is a boolean cofiguration: true for mute the output, false otherwise
+    */
     'secret' => false,
 ];
