c27299490e00f7a38c154e6d93532ed323719a45
Claudiu Nicola <claudiunicola96@gmail.com>
2018-10-01 22:44:49 +0300

    I changed the name of config file.

--- config/laravel-console-dusk.php
+++ config/laravel-console-dusk.php
@@ -9,8 +9,8 @@ return [
     | Here you may configure the name of screenshots and logs directory as you wish.
     */
     'paths' => [
-        'screenshots' => 'screenshots',
-        'log'         => 'log',
+        'screenshots' => storage_path('laravel-console-dusk/screenshots'),
+        'log'         => storage_path('laravel-console-dusk/log'),
     ],
 
     /*
--- src/LaravelConsoleDuskServiceProvider.php
+++ src/LaravelConsoleDuskServiceProvider.php
@@ -18,15 +18,15 @@ class LaravelConsoleDuskServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/console-dusk.php' => config_path('console-dusk.php'),
-                ], 'config');
+                __DIR__ . '/../config/laravel-console-dusk.php' => config_path('laravel-console-dusk.php'),
+                ],'config');
 
             $manager = resolve(ManagerContract::class);
 
             Browser::$baseUrl = config('app.url');
-            Browser::$storeScreenshotsAt = $this->getPath(config('console-dusk.paths.screenshots'));
-            Browser::$storeConsoleLogAt = $this->getPath(config('console-dusk.paths.log'));
-            $inSecret = config('console-dusk.secret');
+            Browser::$storeScreenshotsAt = $this->getPath(config('laravel-console-dusk.paths.screenshots'));
+            Browser::$storeConsoleLogAt = $this->getPath(config('laravel-console-dusk.paths.log'));
+            $inSecret = config('laravel-console-dusk.secret');
 
             Command::macro('browse', function ($callback) use ($manager, $inSecret) {
                 $manager->browse($this, $callback, $inSecret);
@@ -36,7 +36,7 @@ class LaravelConsoleDuskServiceProvider extends ServiceProvider
 
     public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/console-dusk.php', 'console-dusk');
+        $this->mergeConfigFrom(__DIR__ . '/../config/laravel-console-dusk.php', 'laravel-console-dusk');
 
         $this->app->bind(ManagerContract::class, function ($app) {
             return new Manager();
@@ -50,11 +50,9 @@ class LaravelConsoleDuskServiceProvider extends ServiceProvider
 
     protected function getPath(string $path): string
     {
-        return tap(storage_path('laravel-console-dusk'.DIRECTORY_SEPARATOR.$path), function ($path) {
-            foreach ([storage_path(), storage_path('laravel-console-dusk'), $path] as $folder) {
-                if (! File::exists($folder)) {
-                    File::makeDirectory($folder);
-                }
+        return tap($path, function ($path) {
+                if (! File::exists($path)) {
+                    File::makeDirectory($path);
             }
         });
     }
