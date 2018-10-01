cb9a2dbbcd121eba3b501de07339143c6067b334
Claudiu Nicola <claudiunicola96@gmail.com>
2018-10-01 23:50:31 +0300

    Create paths recursively

--- src/LaravelConsoleDuskServiceProvider.php
+++ src/LaravelConsoleDuskServiceProvider.php
@@ -18,8 +18,8 @@ class LaravelConsoleDuskServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__ . '/../config/laravel-console-dusk.php' => config_path('laravel-console-dusk.php'),
-                ],'config');
+                __DIR__.'/../config/laravel-console-dusk.php' => config_path('laravel-console-dusk.php'),
+                ], 'config');
 
             $manager = resolve(ManagerContract::class);
 
@@ -36,7 +36,7 @@ class LaravelConsoleDuskServiceProvider extends ServiceProvider
 
     public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/laravel-console-dusk.php', 'laravel-console-dusk');
+        $this->mergeConfigFrom(__DIR__.'/../config/laravel-console-dusk.php', 'laravel-console-dusk');
 
         $this->app->bind(ManagerContract::class, function ($app) {
             return new Manager();
@@ -51,8 +51,8 @@ class LaravelConsoleDuskServiceProvider extends ServiceProvider
     protected function getPath(string $path): string
     {
         return tap($path, function ($path) {
-                if (! File::exists($path)) {
-                    File::makeDirectory($path);
+            if(! File::exists($path)) {
+                File::makeDirectory($path,0755, true);
             }
         });
     }
